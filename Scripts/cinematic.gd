e2a0f6acc7f88e7852a8435ca1da1f80a65d61b0
TristanBumpus <e2467563@cmaisonneuve.qc.ca>
2026-06-21 12:21:52 -0400

    Final

--- Scripts/cinematic.gd
+++ Scripts/cinematic.gd
@@ -8,7 +8,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("space"):
+		game()
 
 func game():
 	get_tree().change_scene_to_file("res://Levels/level.tscn")
